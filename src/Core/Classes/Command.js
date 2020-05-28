50b82b89b3225e8fba6888b5c6b5061f24d5e365
Boss <boss@staff.libraryofcode.us>
2020-05-28 01:29:48 -0400

    ?

--- src/Core/Classes/Command.js
+++ src/Core/Classes/Command.js
@@ -1,12 +1,12 @@
 class Command { 
     constructor(data) { 
-     this.name = data.name ?? "idiot"; 
+     this.name = data.name 
      this.id = this.name; 
-     this.module = data.module ?? "Some dummy forgot to add a module"
-     this.aliases = data.aliases ?? []; 
-     this.helpDetail = data.helpDetail ?? "Some idiot forgot to add a helpDetail!"; 
-     this.helpUsage = data.helpUsage ?? null; 
-     this.helpExample = data.helpExample ?? null;
+     this.module = data.module 
+     this.aliases = data.aliases 
+     this.helpDetail = data.helpDetail 
+     this.helpUsage = data.helpUsage 
+     this.helpExample = data.helpExample 
 
     }
     async execute(){
