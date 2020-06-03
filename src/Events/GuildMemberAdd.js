819a2fa4bb9d8d07cde650f3ecbdedb1cf33dee0
Boss <boss@staff.libraryofcode.us>
2020-06-02 21:29:04 -0400

    dumb

--- src/Events/GuildMemberAdd.js
+++ src/Events/GuildMemberAdd.js
@@ -21,7 +21,7 @@ client.on('guildMemberAdd', async (guild, member) => {
     }
 
     client.createMessage(config.logChannel, log)
-    client.createMessage('713872016788291604', `Welcome to the server, <@${member.id}>! Please read over <#713872065316388874> and be sure to follow them when in the server! You can find visionwise's streams over in <#713871998563909723>. Enjoy your stay, talk in <#713873296680288266>, and if you have any questions do not hesitate to ask a Moderator+, or DM <@575252669443211264>!`)
-
+    client.createMessage('713872016788291604', `Welcome to the server, <@${member.id}>! Please read over <#717462512664182867> and be sure to follow them when in the server! You can find visionwise's streams over in <#713871998563909723>. Enjoy your stay, talk in <#713873296680288266>, and if you have any questions do not hesitate to ask a Moderator+, or DM <@575252669443211264>!`)
+// asdf
     member.addRole('713881052841771119', 'Automatic autorole.')
 })
