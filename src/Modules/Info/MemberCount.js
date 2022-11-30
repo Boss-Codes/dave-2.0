949e16e1a0f59cb814cce20f607d588ef01f5322
Boss <62577231+Boss-Codes@users.noreply.github.com>
2022-11-30 06:01:38 +0000

    cleaning up membercount, added aliases

--- src/Modules/Info/MemberCount.js
+++ src/Modules/Info/MemberCount.js
@@ -7,6 +7,7 @@ class MemberCount extends Command {
         super({
             name: 'membercount', 
             module: 'Info', 
+            aliases: ['members'],
             userPerms: 'User', 
             helpDetail: 'Shows the amount of members in the server.\nUse the `full` option to show the status breakdown of all members.', 
             helpUsage: `membercount\nmembercount full`
@@ -21,34 +22,19 @@ class MemberCount extends Command {
         let onlineM = msg.channel.guild.members.filter(m => m.status === 'online').length
         let idleM = msg.channel.guild.members.filter(m => m.status === 'idle').length
         let dndM = msg.channel.guild.members.filter(m => m.status === 'dnd').length
-        let presences = msg.channel.guild.maxPresences
+
 
         const data = {
             embed: {
-                author: { 
-                    name: `${msg.channel.guild.name}`,
-                    icon_url: `${msg.channel.guild.iconURL}`
-                }, 
-                footer: {
-                    text: `Server ID: ${msg.channel.guild.id}`
-                }, 
+                author: {  name: `${msg.channel.guild.name}`, icon_url: `${msg.channel.guild.iconURL}` }, 
+                footer: { text: `Server ID: ${msg.channel.guild.id}` }, 
                 timestamp: new Date,
-                color: `${defaultColor}`,
-                fields: [{ 
-                    name: 'Members', 
-                    value: totalMemberCount, 
-                    inline: true
-                }, 
-                {
-                    name: 'Users', 
-                    value: humans, 
-                    inline: true
-                }, 
-                {
-                    name: 'Bots', 
-                    value: bots, 
-                    inline: true
-                }]
+                color: defaultColor,
+                fields: [
+                { name: 'Members', value: totalMemberCount, inline: true }, 
+                { name: 'Users', value: humans, inline: true }, 
+                { name: 'Bots', value: bots, inline: true }
+            ]
             }       
         }
         if (!args[0]) { 
@@ -60,56 +46,21 @@ class MemberCount extends Command {
         if (args.join(' ') === 'full') { 
             client.createMessage(msg.channel.id, {
                 embed: {
-                    author: { 
-                        name: `${msg.channel.guild.name}`,
-                        icon_url: `${msg.channel.guild.iconURL}`
-                    }, 
-                    footer: {
-                        text: `Server ID: ${msg.channel.guild.id}`
-                    }, 
+                    author: { name: `${msg.channel.guild.name}`, icon_url: `${msg.channel.guild.iconURL}` }, 
+                    footer: { text: `Server ID: ${msg.channel.guild.id}` }, 
                     timestamp: new Date,
-                    color: `${defaultColor}`,
-                    fields: [{ 
-                        name: `Online ${online}`, 
-                        value: onlineM, 
-                        inline: true
-                    },
-                    { 
-                        name: `Idle ${idle}`, 
-                        value: idleM, 
-                        inline: true
-                    }, 
-                    {
-                        name: `DnD ${dnd}`, 
-                        value: dndM, 
-                        inline: true
-                    }, 
-                    {
-                        name: 'Users', 
-                        value: totalMemberCount, 
-                        inline: true
-                    }, 
-                    {
-                        name: 'Humans', 
-                        value: humans, 
-                        inline: true
-                    }, 
-                    {
-                        name: 'Bots', 
-                        value: bots, 
-                        inline: true
-                    },
-                    {
-                        name: 'Max Members', 
-                        value: `${msg.channel.guild.maxMembers}`, 
-                        inline: true
-                    }, 
-                    {
-                        name: 'Max Presences', 
-                        value: `${presences}`, 
-                        inline: true
-                    }]
-                }
+                    color: defaultColor,
+                    fields: [
+                    { name: `Online ${online}`, value: onlineM, inline: true },
+                    { name: `Idle ${idle}`, value: idleM, inline: true }, 
+                    { name: `DnD ${dnd}`, value: dndM, inline: true}, 
+                    { name: 'Users', value: totalMemberCount, inline: true }, 
+                    { name: 'Humans', value: humans, inline: true }, 
+                    { name: 'Bots', value: bots, inline: true },
+                    { name: 'Max Members', value: `${msg.channel.guild.maxMembers}`, inline: true }, 
+                    { name: 'Max Presences', value: `N/A`, inline: true }
+                ]
+            }
             })
         }
     }
