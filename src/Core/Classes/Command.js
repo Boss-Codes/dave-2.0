64792e0c5374dc36a1427f6e412b0df0fc770a66
Boss <62577231+Boss-Codes@users.noreply.github.com>
2022-12-12 18:59:40 +0000

    qol x4

--- src/Core/Classes/Command.js
+++ src/Core/Classes/Command.js
@@ -1,12 +1,12 @@
 class Command { 
     constructor(data) { 
-        this.name = data.name
+        this.name = data.name || 'unnamed'
         this.id = this.name
-        this.module = data.module
-        this.aliases = data.aliases 
-        this.userPerms = data.userPerms
-        this.helpDetail = data.helpDetail
-        this.helpUsage = data.helpUsage
+        this.module = data.module || 'default'
+        this.aliases = data.aliases || 'None'
+        this.userPerms = data.userPerms || 'User'
+        this.helpDetail = data.helpDetail || 'Some waz lazy!! :('
+        this.helpUsage = data.helpUsage 
         this.helpExample = data.helpExample
 
     }
